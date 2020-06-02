48203cf4b7e0b71b4d5125837d1d0852c97b17dd
mleader <mleader@redhat.com>
2020-06-02 19:31:30 -0400

    define permissible path to upload storage

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import json
 import os
 
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+results_mnt_path = '/'.join((ROOT_DIR, 'results')
+
+
 @hook('after_request')
 def enable_cors():
     """
     You need to add some headers to request.
-    Don't use the wilde card '*' for Access-Control-Allow-Oirgin in production.
+    Don't use the wildcard '*' for Access-Control-Allow-Oirgin in production.
     """
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Request-With, X-CSRF-Token'
 
 
-# landing, unnecessary
 @route('/', method='GET')
 def root():
     return {
@@ -25,16 +28,19 @@ def root():
 @route('/api', method='POST')
 def upload():
     upload = request.files.get('file')
+    print(upload.filename)
     client_ip = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
     print(f'request is coming from ip {client_ip}')
-    print(upload.filename)
     name, ext = os.path.splitext(upload.filename)
     if ext not in ('.png', '.tar.gz', '.jpeg', '.jpg'):
         return 'File extension not allowed.'
-
-    upload.save('/var/bottle-storage') # appends upload.filename automatically
+    
+    # appends upload.filename automatically
+    upload.save(
+        destination = results_mnt_path)
+    ) 
     return 'OK'
 
-
+    
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080, debug=True)
