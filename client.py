28a48c7467575b05ad423bc45f34d9d25b171cb1
mleader <mleader@redhat.com>
2020-06-02 19:31:58 -0400

    define api url

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import sys
 import requests
 
+api_url = 'http://localhost:8080/api'
+
 print(
-    requests.post('api_url', files = {'file': open(sys.argv[1], 'rb')})
+    requests.post(
+        api_url, 
+        files = {'file': open(sys.argv[1], 'rb')})
 )
