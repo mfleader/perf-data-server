54b825ca4ba4df995b8fef626839f6df70450015
mleader <mleader@redhat.com>
2020-06-03 16:33:40 -0400

    add code to post two files from cli

--- client.py
+++ client.py
@@ -3,8 +3,17 @@ import requests
 
 api_url = 'http://localhost:8080/api'
 
-print(
-    requests.post(
-        api_url, 
-        files = {'file': open(sys.argv[1], 'rb')})
-)
+with open(sys.argv[1], 'rb') as f:
+    print(
+        requests.post(
+            api_url, 
+            files = {'file': f})
+    )
+
+# print(
+#     requests.post(
+#         api_url, 
+#         files = {
+#             'file': open(sys.argv[1], 'rb'),
+#             'b': open(sys.argv[2], 'rb')})
+# )
