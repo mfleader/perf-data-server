048c25f7b4eebbb37f7ee9e673aa3a846a6a6458
mleader <mleader@redhat.com>
2020-06-05 15:03:54 -0400

    successful upload to correct directory

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from bottle import route, run, hook, response, request
-import datetime, os, pathlib
-import pprint
+import bottle
+from bottle import response, request
+import datetime, os, pathlib, pprint
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+os.environ['WORKLOAD_TYPE'] = 'devtest'
+os.environ['TRIAL_NAME'] = 't0'
 
 
-@hook('after_request')
+@bottle.hook('after_request')
 def enable_cors():
     """
     You need to add some headers to request.
@@ -17,30 +19,18 @@ def enable_cors():
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Request-With, X-CSRF-Token'
 
 
-@route('/', method='GET')
+@bottle.get('/')
 def root():
     return {
         'api': 'api/'
     }
 
 
-def user():
-    return 'mleader'
-
-
-def prefix():
-    return 'test'
-
-
-def suffix():
-    return 'smoke'
-
-
 def compose_filepath():
     return ''
 
 
-@route('/api', method='POST')
+@bottle.post('/api')
 def upload():
     # get all file keys
     # request.files.keys()
@@ -54,10 +44,14 @@ def upload():
 
     name, ext = os.path.splitext(upload.filename)
     if ext not in {'.png', '.tar.gz', '.jpeg', '.jpg'}:
-        return 'File extension not allowed.'
+        # return 'File extension not allowed.'
+        bottle.abort(400, 'File extennsion not allowed.')
 
     # print(request.forms.get('username'))
     # print(request.forms.get('clustername'))
+
+    print(request.environ.get('WORKLOAD_TYPE'))
+    print(request.environ.get('TRIAL_NAME'))
 
     # can get username and clustername from oc cluster-info
     filepath = '/'.join((
@@ -65,22 +59,36 @@ def upload():
         'results',
         client_ip,
         '-'.join((
-            request.environ.get('WORKLOAD_TYPE'),
+            workload_type(request),
             datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S'),
-            request.environ.get('TRIAL_NAME')
+            trial_name(request)
         ))
     ))
     print(filepath)
 
-    # pathlib.Path(filepath) \
-    #     .mkdir(parents=True, exist_ok=True)
+    pathlib.Path(filepath) \
+        .mkdir(parents=True, exist_ok=True)
     
-    # appends upload.filename automatically
-    # upload.save(
-    #     destination = filepath
-    # ) 
+    # # appends upload.filename automatically
+    upload.save(destination = filepath) 
     return 'OK'
+
+
+def workload_type(request):
+    env_var = request.environ.get('WORKLOAD_TYPE')
+    if env_var: # exists
+        return env_var
+    # else    
+    bottle.abort(400, 'Missing environment variable WORKLOAD_TYPE.')
+
+
+def trial_name(request):
+    env_var = request.environ.get('TRIAL_NAME')
+    if env_var: # exists
+        return env_var
+    # else    
+    bottle.abort(400, 'Missing environment variable TRIAL_NAME.')
 
     
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    bottle.run(host='0.0.0.0', port=8080, debug=True)
